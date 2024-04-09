c7871dc1a7bbd33a793260206cde2a1eafff3fd5
CodeBro <120492459+WaqasZafar9@users.noreply.github.com>
2024-04-09 12:55:07 +0500

    Create print all days.dart
    Create an empty list of type string called days. Use the add method to add names of 7 
    days and print all days.

---
+++ print all days.dart
@@ -0,0 +1,7 @@
+void main() {
+ List<String> days = [];
+ days.addAll(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
+'Saturday', 'Sunday']);
+ print('Days of the Week:');
+ print(days);
+}
